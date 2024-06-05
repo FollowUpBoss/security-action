ce8e13bee7937ed98497e77d06936284a06c731c
James Luther <81767471+james-luther@users.noreply.github.com>
2024-06-05 12:34:21 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ if [ -z "${GITHUB_TOKEN-}" ]; then
 fi
 
 # Run Trivy and generate results
-trivy rootfs --format json --scanners vuln,misconfig,secret -o report.json .
+trivy rootfs --format json --scanners misconfig -o report.json .
 
 # Parse and create issues
 python /trivy_parser.py report.json
