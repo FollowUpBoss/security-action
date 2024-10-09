87efea76538e7c938fec414f3120b8d3581f805c
James Luther <james@b34rd.tech>
2024-10-09 16:25:45 -0500

    Add git diff

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,23 +6,22 @@ if [ -z "${GITHUB_TOKEN-}" ]; then
 	exit 1
 fi
 
-echo $GITHUB_REF
-echo $GITHUB_REF_NAME
 
-PR=`echo $GITHUB_REF_NAME | cut -d '/' -f1`
+git diff origin/main origin/${GITHUB_HEAD_REF} > diff
+#PR=`echo $GITHUB_REF_NAME | cut -d '/' -f1`
 
 # Get list of files changed by the PR Branch
-files='$(curl -L \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-  "https://api.github.com/repos/${GITHUB_REPOSITORY}/PULLS/${PR}/files" | jq ".[] | .filename")'
+#files='$(curl -L \
+#  -H "Accept: application/vnd.github+json" \
+#  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+#  "https://api.github.com/repos/${GITHUB_REPOSITORY}/PULLS/${PR}/files" | jq ".[] | .filename")'
 
 # Copy the modified/added files to a folder
-mkdir files
-for file in $files; do cp "$file" files; done
+#mkdir files
+#for file in $files; do cp "$file" files; done
 
 # Run Trivy and generate results
-trivy rootfs --format json --scanners misconfig --ignorefile .circleci/security/.trivyignore.yaml -o report.json ./files
+trivy rootfs --format json --scanners misconfig --ignorefile .circleci/security/.trivyignore.yaml -o report.json diff
 
 # Parse and create issues
 python /trivy_parser.py report.json
