78ad9dca9a50c33dd2d872a32e96889d8029e17d
James Luther <james@b34rd.tech>
2024-10-09 13:24:33 -0500

    Update to generate files modified or added folder for scanning

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@
 FROM ghcr.io/aquasecurity/trivy:0.54.1
 COPY entrypoint.sh /
 COPY trivy_parser.py /
-RUN apk --no-cache add bash curl npm python3 py3-pygithub
+RUN apk --no-cache add bash curl npm python3 py3-pygithub jq
 RUN chmod +x /entrypoint.sh
 RUN chmod +x /trivy_parser.py
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -9,14 +9,17 @@ fi
 PR=`echo $GITHUB_REF_NAME | cut -d '/' -f1`
 
 # Get list of files changed by the PR Branch
-curl -L \
+files=`curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  "https://api.github.com/repos/${GITHUB_REPOSITORY}/PULLS/${PR}/files"
+  "https://api.github.com/repos/${GITHUB_REPOSITORY}/PULLS/${PR}/files" | jq '.[] | .filename'`
+
+# Copy the modified/added files to a folder
+mkdir files
+for file in $files; do cp "$file" files; done
 
 # Run Trivy and generate results
-trivy rootfs --format json --scanners misconfig --ignorefile .circleci/security/.trivyignore.yaml -o report.json .
+trivy rootfs --format json --scanners misconfig --ignorefile .circleci/security/.trivyignore.yaml -o report.json ./files
 
 # Parse and create issues
 python /trivy_parser.py report.json
