677e5715d5998ca7b7dd3cb334b1210d334cba9b
James Luther <james@b34rd.tech>
2024-10-09 12:37:42 -0500

    Remove temporary check

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,14 +6,14 @@ if [ -z "${GITHUB_TOKEN-}" ]; then
 	exit 1
 fi
 
-echo $GITHUB_REF_NAME
+PR=`echo $GITHUB_REF_NAME | cut -d '/' -f1`
 
 # Get list of files changed by the PR Branch
-#curl -L \
-#  -H "Accept: application/vnd.github+json" \
-#  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-#  -H "X-GitHub-Api-Version: 2022-11-28" \
-#  "https://api.github.com/repos/${GITHUB_REPOSITORY}/PULLS/${PULL_NO}/files"
+curl -L \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  "https://api.github.com/repos/${GITHUB_REPOSITORY}/PULLS/${PR}/files"
 
 # Run Trivy and generate results
 trivy rootfs --format json --scanners misconfig --ignorefile .circleci/security/.trivyignore.yaml -o report.json .
