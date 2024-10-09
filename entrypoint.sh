277da26bbe9b0239e90d414372bb4e48bc7272aa
James Luther <james@b34rd.tech>
2024-10-09 12:30:47 -0500

    Temporary check for syntax of GIT_REF_NAME env variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,17 @@ if [ -z "${GITHUB_TOKEN-}" ]; then
 	exit 1
 fi
 
-git diff --no-index origin > diff
+echo $GITHUB_REF_NAME
+
+# Get list of files changed by the PR Branch
+#curl -L \
+#  -H "Accept: application/vnd.github+json" \
+#  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+#  -H "X-GitHub-Api-Version: 2022-11-28" \
+#  "https://api.github.com/repos/${GITHUB_REPOSITORY}/PULLS/${PULL_NO}/files"
 
 # Run Trivy and generate results
-trivy rootfs --format json --scanners misconfig --ignorefile .circleci/security/.trivyignore.yaml -o report.json diff
+trivy rootfs --format json --scanners misconfig --ignorefile .circleci/security/.trivyignore.yaml -o report.json .
 
 # Parse and create issues
 python /trivy_parser.py report.json
