d55402fcf3ad5124d9bdb1dc821c116bc71b741b
James Luther <james@b34rd.tech>
2024-10-10 15:48:36 -0500

    Edit syntax of entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
+set -eu
 
+if [ -z "${GITHUB_TOKEN-}" ]; then
+	echo "GITHUB_TOKEN must be set."
+	exit 1
+fi
+
+scanType="${INPUT_SCAN_TYPE:-branch}"
+
+# Run Trivy and generate results
+if [ "${scanType}" == "diff" ]; then
+  trivy rootfs --format json --scanners misconfig --ignorefile .circleci/security/.trivyignore.yaml -o report.json diff
+elif [  "${scanType}" == "branch" ]; then
+  trivy rootfs --format json --scanners misconfig --ignorefile .circleci/security/.trivyignore.yaml -o report.json .
+fi
+
+# Parse and create issues
+python /trivy_parser.py report.json
